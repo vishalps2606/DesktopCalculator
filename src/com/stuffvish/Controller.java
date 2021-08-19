708538fb657a7cdaa501741df948410dff44347d
Vinícius Maffioli <56659378+vmaffioli@users.noreply.github.com>
2021-08-19 04:28:19 -0300

    Update Controller
    Fix issue: Calculator doesn't work properly #1.
    Del-> boolean vars and choicebox listener,
    Add-> choicebox value validation on calculate method

--- src/com/stuffvish/Controller.java
+++ src/com/stuffvish/Controller.java
@@ -9,90 +9,80 @@ import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
-    @FXML
-    public Label label1, label2, label3;
-    @FXML
-    public TextField textField1, textField2;
-    @FXML
-    public ChoiceBox<String> choiceBox;
-    @FXML
-    public Button resultButton;
+	@FXML
+	public Label label1, label2, label3;
+	@FXML
+	public TextField textField1, textField2;
+	@FXML
+	public ChoiceBox<String> choiceBox;
+	@FXML
+	public Button resultButton;
 
-    private static final String add = "ADD";
-    private static final String substract = "SUBSTRACT";
-    private static final String multiply = "MULTIPLY";
-    private static final String divide = "DIVIDE";
-    private static final String modulus = "MODULUS";
+	private static final String add = "ADD";
+	private static final String substract = "SUBSTRACT";
+	private static final String multiply = "MULTIPLY";
+	private static final String divide = "DIVIDE";
+	private static final String modulus = "MODULUS";
 
-    private boolean isAdd;
-    private boolean isSub;
-    private boolean isMul;
-    private boolean isDiv;
-    private boolean isMod;
+	
+	@Override
+	public void initialize(URL url, ResourceBundle resourceBundle) {
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+		choiceBox.getItems().add(add);
+		choiceBox.getItems().add(substract);
+		choiceBox.getItems().add(multiply);
+		choiceBox.getItems().add(divide);
+		choiceBox.getItems().add(modulus);
+		choiceBox.setValue(add);
 
-        choiceBox.getItems().add(add);
-        choiceBox.getItems().add(substract);
-        choiceBox.getItems().add(multiply);
-        choiceBox.getItems().add(divide);
-        choiceBox.getItems().add(modulus);
-        choiceBox.setValue(add);
+		resultButton.setOnAction(event -> {
+			System.out.println("Button Clicked");
+			calculate();
+			// more function
+		});
+	}
 
-        choiceBox.getSelectionModel().selectedItemProperty().addListener((observableValue, s, t1) -> {
-                System.out.println(s + " changed to " + t1);
-                if (t1.equals(add)) isAdd = true;
-                else  if (t1.equals(substract)) isSub = true;
-                else  if (t1.equals(multiply)) isMul = true;
-                else  if (t1.equals(divide)) isDiv = true;
-                else isMod = true;
-        });
+	private void calculate() {
+		String input1 = textField1.getText();
+		String input2 = textField2.getText();
 
-        resultButton.setOnAction(event -> {
-            System.out.println("Button Clicked");
-            calculate();
-            //more function
-        });
-    }
+		try {
+			float result = 0;
+			float enteredNumber1 = Float.parseFloat(input1);
+			float enteredNumber2 = Float.parseFloat(input2);
 
-    private void calculate() {
-        String input1 = textField1.getText();
-        String input2 = textField2.getText();
+			if (choiceBox.getSelectionModel().getSelectedItem().equals(add))
+				result = enteredNumber1 + enteredNumber2;
+			else if (choiceBox.getSelectionModel().getSelectedItem().equals(substract))
+				result = enteredNumber1 - enteredNumber2;
+			else if (choiceBox.getSelectionModel().getSelectedItem().equals(multiply))
+				result = enteredNumber1 * enteredNumber2;
+			else if (choiceBox.getSelectionModel().getSelectedItem().equals(divide))
+				result = enteredNumber1 / enteredNumber2;
+			else if (choiceBox.getSelectionModel().getSelectedItem().equals(modulus))
+				result = (enteredNumber1*100) / enteredNumber2;
 
-        try {
-            float result;
+			display(result);
 
-            float enteredNumber1 = Float.parseFloat(input1);
-            float enteredNumber2 = Float.parseFloat(input2);
+		} catch (Exception exception) {
+			warnUser();
+		}
+	}
 
-            if (isAdd) result = enteredNumber1 + enteredNumber2;
-            else if (isSub) result = enteredNumber1 - enteredNumber2;
-            else if (isMul) result = enteredNumber1 * enteredNumber2;
-            else if (isDiv) result = enteredNumber1 / enteredNumber2;
-            else result = enteredNumber1 % enteredNumber2;
+	private void warnUser() {
+		Alert alert = new Alert(Alert.AlertType.WARNING);
+		alert.setTitle("Warning");
+		alert.setHeaderText("Warning");
+		alert.setContentText("Please Enter valid number");
+		alert.show();
+	}
 
-            display(result);
-
-        }catch (Exception exception){
-            warnUser();
-        }
-    }
-
-    private void warnUser() {
-        Alert alert = new Alert(Alert.AlertType.WARNING);
-        alert.setTitle("Warning");
-        alert.setHeaderText("Warning");
-        alert.setContentText("Please Enter valid number");
-        alert.show();
-    }
-
-    private void display(float result) {
-        System.out.println("The result is " + result);
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Result");
-        alert.setHeaderText("Result");
-        alert.setContentText("The result is " + result);
-        alert.show();
-    }
+	private void display(float result) {
+		System.out.println("The result is " + result);
+		Alert alert = new Alert(Alert.AlertType.INFORMATION);
+		alert.setTitle("Result");
+		alert.setHeaderText("Result");
+		alert.setContentText("The result is " + result);
+		alert.show();
+	}
 }
